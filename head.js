dca8cb0ea5998510fcec83bf43ee776eaeef139d
VaishnaviMadhave <102609138+VaishnaviMadhave@users.noreply.github.com>
2024-06-28 18:58:12 +0530

    Add files via upload

---
+++ head.js
@@ -0,0 +1,22 @@
+
+function show(){
+  document.getElementsByClass("store").style.animation="store 2s linear infinite 7s";
+}
+
+  /*  const hp = new Swiper(".hp",
+ {
+    loop: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centerSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      depth: 250,
+      modifier: 1,
+      scale: 0.75,
+      slideShadows: false,
+      stretch: -100,
+    },
+   
+    });*/
